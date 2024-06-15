19c47e0414e19d8865acf6e9750b75629594f138
a-simple-software-dev-team <750144276@qq.com>
2024-06-15 16:39:43 +0800

    second

--- src/components/tutors_certification.vue
+++ src/components/tutors_certification.vue
@@ -1,36 +1,36 @@
 <template>
 
-    <div class="certification">
-        <br>
-        <br>
-        <br>
-        <br>
-        <span>家教资格认证:     </span>  &nbsp;&nbsp;
-        <el-input v-model="tutor_id" style="width: 240px" placeholder="Please input" />&nbsp;&nbsp;
-        <br>
-        <br>
-        <br>
-        <el-button plain @click="open">查询</el-button>
-        <br>
-        <br>
-        <br>
-
-        
+  <div class="certification">
+    <br>
+    <br>
+    <br>
+    <br>
+    <span>家教资格认证: </span> &nbsp;&nbsp;
+    <el-input v-model="tutor_id" style="width: 240px" placeholder="Please input" />&nbsp;&nbsp;
+    <br>
+    <br>
+    <br>
+    <el-button plain @click="open">查询</el-button>
+    <br>
+    <br>
+    <br>
 
 
-    </div>
-    
-  </template>
-  
-  <script  setup>
-  import { ref } from 'vue'
-  const tutor_id = ref('')
-  import { ElMessage, ElMessageBox } from 'element-plus'
-    import { Action } from 'element-plus'
 
-    const open = () => {
+
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const tutor_id = ref('')
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { Action } from 'element-plus'
+
+const open = () => {
   ElMessageBox.alert('This is a message', '查询结果', {
-  
+
     confirmButtonText: 'OK',
     callback: (action) => {
       ElMessage({
@@ -41,13 +41,12 @@
   })
 }
 
-  </script>
-  <style>
-         .certification {
-            position: relative;
-            height: 400px;
-            border: 2px solid #071a52;
+</script>
+<style>
+.certification {
+  position: relative;
+  height: 400px;
+  border: 2px solid #071a52;
 
-    }
-
+}
 </style>
